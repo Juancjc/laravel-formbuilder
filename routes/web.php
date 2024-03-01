b65673763fa5985f300e192d054ab0b840f8000b
juan.coelho <juan.coelho@ac.gov.br>
2024-02-29 22:19:12 -0500

    form mandando o request como desejado

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ForumularioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('formulario', ForumularioController::class);
+
 Route::get('/formIO', function () {
     return view('formIO');
 });
